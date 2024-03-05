7bf4dadc6b5f76f6f0a50eb9d93ee4371231137e
RomanZubcov <roman.zubcov19@gmail.com>
2024-03-05 08:16:00 +0200

    new changes made in text format

--- UDS/vCan_on.sh
+++ UDS/vCan_on.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Deschide un nou terminal și execută comenzile următoare în el
+# Deschide un nou terminal si executa comenzile urmatoare în el
 gnome-terminal -- bash -c "
 # Configurarea unui nou dispozitiv virtual CAN numit vcan0
 sudo modprobe vcan;
 sudo ip link add dev vcan0 type vcan;
 sudo ip link set up vcan0;
 
-echo 'Interfața Virtual CAN (vcan0) a fost creată și activată.';
+echo 'Interfata Virtual CAN (vcan0) a fost creata si activata.';
 
-# Folosește candump pentru a asculta pe interfața vcan0
-echo 'Se așteaptă mesaje pe vcan0...';
+# Foloseste candump pentru a asculta pe interfata vcan0
+echo 'Se asteaptă mesaje pe vcan0...';
 candump vcan0;
 "
 
